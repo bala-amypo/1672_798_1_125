ab7700ffc4af3d9b143e5ddd57e5e5feba2f190d
bala-amypo <bala@amypo.in>
2025-12-18 08:22:10 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CategoryServiceImpl.java
@@ -1,5 +1,5 @@
-package com.example.demo.service;
-
+package com.example.demo.service.impl;
+import com.example.demo.service.CategoryService;
 import com.example.demo.entity.Category;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.CategoryRepository;
