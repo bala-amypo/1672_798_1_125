31e2ade8be3b3286fd25d6c1a67ef8bb401e7aad
bala-amypo <bala@amypo.in>
2025-12-21 17:09:48 +0000

    User commit

--- src/main/java/com/example/demo/repository/MenuItemRepository.java
+++ src/main/java/com/example/demo/repository/MenuItemRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 
 public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
 
-    // 🔴 REQUIRED by service + tests
+   
     Optional<MenuItem> findByNameIgnoreCase(String name);
 
     @Query("SELECT DISTINCT m FROM MenuItem m LEFT JOIN FETCH m.categories WHERE m.active = true")
